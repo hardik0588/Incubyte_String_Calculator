04bf8ae465a6ea4b2540f787101db2d911c8adc4
Hardik Mer <hardikmer3042001@gmail.com>
2022-08-21 19:13:46 +0530

    Add testcast for empty input value
    This test checks add method of class StringCalculator should return 0 if enter String is empty and add code in method add for to  pass this test

--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -1,3 +1,16 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class StringCalculatorTest {
     
+    @Test
+    public void hasReturnZeroIfStingIsEmpty() {
+        StringCalculator stringCalculator = new StringCalculator();
+        String inputValue = "";
+        int expectedValue = 0;
+        int actualValue = stringCalculator.add(inputValue);
+        assertEquals(expectedValue, actualValue);
+    }
+
 }
