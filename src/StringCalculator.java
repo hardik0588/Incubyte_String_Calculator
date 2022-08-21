4bd44dd1cdc862743e9cd555dd5f622b2bf82b81
Hardik Mer <hardikmer3042001@gmail.com>
2022-08-21 19:50:53 +0530

    Add TestCase for 1 value
    This test check add method of StringCalculator should return value 0 if enter String has single Value and add code for pass this test

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,16 @@
+import java.util.StringTokenizer;
+
 public class StringCalculator {
     public  int add(String numbers)
     {
         if(numbers.equals(""))
         {
             return 0;
+        }
+        StringTokenizer st=new StringTokenizer(numbers,",");
+        if(st.countTokens()==1)
+        {
+            return Integer.parseInt(numbers);
         }
         return -1;
     }  
--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -1,16 +1,29 @@
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalculatorTest {
-    
+
+    private StringCalculator stringCalculator;
+
+    @Before
+    public void initForTests() {
+        stringCalculator = new StringCalculator();
+    }
+
     @Test
     public void hasReturnZeroIfStingIsEmpty() {
-        StringCalculator stringCalculator = new StringCalculator();
         String inputValue = "";
         int expectedValue = 0;
         int actualValue = stringCalculator.add(inputValue);
         assertEquals(expectedValue, actualValue);
     }
-
+    @Test
+    public void hasOneValues() {
+        String inputValue = "1";
+        int expectedValue = 1;
+        int actualValue = stringCalculator.add(inputValue);
+        assertEquals("it should return one if it has one arugment", expectedValue, actualValue);
+    }
 }
